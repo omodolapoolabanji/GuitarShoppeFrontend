d65a1614f5b24abbc9b1f09922f76eaf81023abc
omodolapoolabanji <omodolapoolabanji@gmail.com>
2024-06-18 01:59:04 -0400

    newcommit

--- guitar-center-frontend/src/services/service.jsx
+++ guitar-center-frontend/src/services/service.jsx
@@ -1,8 +1,8 @@
-var hostState = 0; 
+var hostState = 1; 
 
 export function getHost(){
 
     {/**prod and dev hosts  */}
-    const host = hostState === 0 ? 'http://localhost:8080/' : '' 
+    const host = hostState === 0 ? 'http://localhost:8080/' : 'https://guitarshoppe.onrender.com/' 
     return host
 }
